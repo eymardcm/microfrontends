e5f9c0a3b1ead3c601d33dda05b648cf3000ed3a
Chad Eymard <chad.eymard@coop.org>
2023-04-05 16:54:50 -0400

    small change to test cicd

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from "react"
 
-import MarketingApp from "./components/MarketingApp";
+import MarketingApp from "./components/MarketingApp"
 
 export default () => {
     return <div>
