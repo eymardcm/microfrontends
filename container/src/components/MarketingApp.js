f73ff58b4e622e3ce923abc54d271161a92dc96c
Chad Eymard <chad.eymard@coop.org>
2023-04-05 15:59:21 -0400

    init

---
+++ container/src/components/MarketingApp.js
@@ -0,0 +1,13 @@
+import React, {useRef, useEffect } from 'react';
+import { mount as mountMarketingApp } from 'marketing/MarketingApp';
+
+export default () => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        mountMarketingApp(ref.current)
+    })
+
+
+    return <div ref={ref}></div>
+}
